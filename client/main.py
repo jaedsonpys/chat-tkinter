c0c3a90b26dd93c89361d1696cacf005d8a0adc4
Jaedson Silva <contacanal798@gmail.com>
2022-02-05 18:19:19 -0300

    criando interface basica

--- client/main.py
+++ client/main.py
@@ -1,7 +1,37 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
+
 root.geometry('400x400')
-root.title('Primeira janela')
+root.title('Chat Tkinter - @jaedsonpys')
+root.resizable(width=0, height=0)
+
+root.grid_columnconfigure(0, weight=1)
+
+# set_name_frame
+set_name_frame = Frame(root)
+
+Label(set_name_frame, text='Seu nome').grid(row=1, column=1)
+
+name_entry = Entry(set_name_frame)
+name_entry.grid(row=1, column=2)
+
+Button(set_name_frame, text='Salvar').grid(row=1, column=3)
+
+set_name_frame.grid_columnconfigure(1, weight=1)
+set_name_frame.grid(row=0, column=0)
+
+# message_frame
+message_frame = Frame(root)
+
+message_list = Listbox(
+    message_frame,
+    background='white',
+    height=17
+)
+
+message_frame.grid(row=2, sticky='nesw')
+message_list.pack(fill=BOTH, expand=True, pady=10, padx=10)
 
 root.mainloop()
