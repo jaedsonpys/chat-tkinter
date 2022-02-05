fd277e8906eb87eb98fde0216511dd43dd40a0e6
Jaedson Silva <contacanal798@gmail.com>
2022-02-05 00:16:32 -0300

    criando metodo _broadcast_message()

--- server.py
+++ server.py
@@ -36,6 +36,20 @@ class Server:
     def _register_user(self, username: str, address: str,) -> None:
         self.clients[address] = username
 
+    def _broadcast_message(self, message: str, sender_addr: str) -> None:
+        sender = self.clients.get(sender_addr)
+
+        message_info = {'type': 'message', 
+                        'sender': sender,
+                        'message': message}
+
+        message_json = json.dumps(message_info, ensure_ascii=False)
+
+        for addr in self.clients.keys():
+            if addr != sender_addr:
+                address = addr.split(':')
+                self._sock.sendto(message_json.encode(), address)
+
     def _receive_connections(self) -> None:
         while True:
             msg, addr = self._sock.recvfrom(1024)
