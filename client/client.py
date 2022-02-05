7121aed596d2cb2249d747b9631adb8cc9cc19e2
Jaedson Silva <contacanal798@gmail.com>
2022-02-05 19:26:06 -0300

    adicionando metodo para enviar mensagens

--- client/client.py
+++ client/client.py
@@ -1,10 +1,17 @@
 import socket
+import json
 
 
 class Client:
     def __init__(self) -> None:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.addr = ('192.168.0.111', 3000)
+
+    def send_message(self, message: str):
+        message_json = json.dumps({'type': 'message',
+                        'content': message}, ensure_ascii=False)
+
+        self._sock.sendto(message.encode(), self.addr)
 
     def receive_broadcast(self):
         message = self._sock.recv(5024)
