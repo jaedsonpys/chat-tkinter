fe1d68bc95f12a5d49d59541cb92cf710a9dcd6d
Jaedson Silva <contacanal798@gmail.com>
2022-02-05 20:44:23 -0300

    implementando ping em Client()

--- client/client_conn.py
+++ client/client_conn.py
@@ -3,9 +3,22 @@ import json
 
 
 class Client:
-    def __init__(self) -> None:
+    def __init__(self) -> bool:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.addr = ('192.168.0.111', 3000)
+
+        # ping server
+        ping = json.dumps({'type': 'ping'})
+        self._sock.sendto(ping.encode(), self.addr)
+
+        # wait response
+        self._sock.settimeout(3.0)
+        pong = self._sock.recv(1024).decode()
+
+        if pong != 'pong':
+            return False
+
+        return True
 
     def register_user(self, username: str):
         message_json = json.dumps({'type': 'register', 'username': username}, ensure_ascii=False)
